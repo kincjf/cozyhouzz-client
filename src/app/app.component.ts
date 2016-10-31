14ad235a2398442e88d6dce11a6af994988f7b54
KIMSEONHO <sinho0689@gmail.com>
2016-11-01 01:50:39 +0900

    - change index.html and app.component.ts
    Signed-off-by: KIMSEONHO <sinho0689@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -54,11 +54,11 @@ export class AppComponent {
   public loadScript() {
     const url = "assets/js-canvas/functions.js";
 
-    console.log('preparing to load...')
+    console.log('preparing to load...');
     let node = document.createElement('script');
     node.src = url;
     node.type = 'text/javascript';
-    node.async = true;
+    node.defer = true;
     node.charset = 'utf-8';
     document.getElementsByTagName('head')[0].appendChild(node);
   }
