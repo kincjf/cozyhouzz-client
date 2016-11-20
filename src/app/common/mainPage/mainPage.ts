755a21a8b891b5727ee1593c4082aa141c11b96d
joyounghyeon <jocompany1991@gmail.com>
2016-11-21 01:22:11 +0900

    메인페이지 수정
    Signed-off-by: joyounghyeon <jocompany1991@gmail.com>

--- src/app/common/mainPage/mainPage.ts
+++ src/app/common/mainPage/mainPage.ts
@@ -4,6 +4,7 @@ import {Http} from '@angular/http'
 import {contentHeaders} from '../../common/headers';
 import * as _ from 'lodash';
 import { config } from '../../common/config';
+import {STATIC_VALUE} from "../config/staticValue";
 
 // const _ = require('lodash');
 const template = require('./mainPage.html');
@@ -21,7 +22,6 @@ export class MainPage {
   serverHost: string;
   returnedDatas = [];
 
-
   constructor(public router: Router, public http: Http) {
 
     this.pageSize = 8;
@@ -35,8 +35,20 @@ export class MainPage {
           this.serverHost = config.serverHost;
 
           //for of문으로 for~of 루프구문은 배열의 요소들, data 순회하기 위한구문
-          for (var buildCaseInfo of response.buildCaseInfo) {
-            this.returnedDatas.push(buildCaseInfo);
+          for (var buildCaseData of response.buildCaseInfo) {
+            let bulidPlaceArr = JSON.parse(buildCaseData.buildPlace);
+            let key = _.findKey(STATIC_VALUE.PLACE_TYPE, ["number", buildCaseData.buildType]);
+
+            this.returnedDatas.push({
+              idx: buildCaseData.idx,
+              title: buildCaseData.title,
+              mainPreviewImage: buildCaseData.mainPreviewImage,
+              buildTotalArea: buildCaseData.buildTotalArea,
+              buildType: STATIC_VALUE.PLACE_TYPE[key].name,
+              buildTotalPrice: buildCaseData.buildTotalPrice,
+              buildPlace: bulidPlaceArr[1],
+              buildPlaceDetail: bulidPlaceArr[2]
+            });
           }
         },
         error => {
